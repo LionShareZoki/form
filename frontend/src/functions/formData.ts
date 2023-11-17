a90eb049825f148c0d2fc1eff8139618d16e4146
LionShareZoki <pujic.zoran@gmail.com>
2023-11-17 14:44:25 +0100

    Delete unnecessary lines

--- frontend/src/functions/formData.ts
+++ frontend/src/functions/formData.ts
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { RootState } from "../redux/rootReducer";
 
 export const getUserDataFromStore = () => {
-  // Use the useSelector hook to access the Redux store state
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const formData = useSelector((state: RootState) => state.form["formData"]);
 
