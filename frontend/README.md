7fb238b2a7171e849ec5e6401aa4de3ed887656d
LionShareZoki <pujic.zoran@gmail.com>
2023-11-17 14:35:52 +0100

    Init Front-end commit

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# From Project
+
+This is a form with validation created in:
+-React
+-Typescript
+-NodeJS/ExpressJS
+-Redux
+-SQLLite DB
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import HomePage from "./pages/HomePage";
+import "./App.scoped.css";
+
+function App() {
+  return (
+    <>
+      <div className="appWrapper">
+        <HomePage />
+      </div>
+    </>
+  );
+}
+
+export default App;
