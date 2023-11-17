f7dfb99e829e86a1ecac77bc5ac330439da11fac
LionShareZoki <pujic.zoran@gmail.com>
2023-11-17 14:42:11 +0100

    Add root reducer

---
+++ frontend/src/redux/rootReducer.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import formReducer from "./reducers";
+
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
