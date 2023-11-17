2bbeaa4ceb270a6e1ad71efadaad8b973b000d7a
LionShareZoki <pujic.zoran@gmail.com>
2023-11-17 14:33:16 +0100

    Create iUserRepository interface

---
+++ backend/src/interfaces/iUserRepository.ts
@@ -0,0 +1,7 @@
+import { User } from "../data/models/user";
+
+export interface IUserRepository {
+  getAllUsers(): Promise<User[]>;
+  insertUser(user: User): Promise<void>;
+  getByEmail(email: string): Promise<User>;
+}
