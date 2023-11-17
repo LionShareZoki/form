012b8d049ea215efc24c2c57c0ce897b3dec245f
LionShareZoki <pujic.zoran@gmail.com>
2023-11-17 14:41:07 +0100

    add form types

---
+++ frontend/src/redux/formTypes.ts
@@ -0,0 +1,22 @@
+export const SUBMIT_FORM = "SUBMIT_FORM";
+export const RESET_FORM = "RESET_FORM";
+
+interface SubmitFormAction {
+  type: typeof SUBMIT_FORM;
+  payload: FormData;
+}
+
+interface ResetFormAction {
+  type: typeof RESET_FORM;
+}
+
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  address: string;
+  phone: string;
+  email: string;
+  isChecked: boolean;
+}
+
+export type FormAction = SubmitFormAction | ResetFormAction;
