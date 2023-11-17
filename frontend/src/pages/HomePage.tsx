0e497b9370a251cb033c9f513f49e165f7e79ccb
LionShareZoki <pujic.zoran@gmail.com>
2023-11-17 14:40:24 +0100

    Create HomePage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Form from "../components/form/Form";
+import "./HomePage.scoped.css";
+
+function HomePage() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default HomePage;
